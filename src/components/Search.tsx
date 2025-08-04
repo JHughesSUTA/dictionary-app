10e091b650f3c50dc0e92cae7353120f1ca8b6ad
James Hughes <james.a.hughes@gmail.com>
2025-08-04 09:59:22 -0500

    search handle error and accessibility updates

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import type { ChangeEvent, FormEvent } from "react";
+import { useTheme } from "../contexts/ThemeContext";
 import SearchIcon from "./icons/SearchIcon";
 import { useNavigate } from "react-router-dom";
 
 const Search = () => {
   const navigate = useNavigate();
   const [searchTerm, setSearchTerm] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  const { theme } = useTheme();
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(e.target.value);
@@ -13,10 +17,15 @@ const Search = () => {
 
   const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (searchTerm.trim()) {
-      navigate(`/${searchTerm.toLowerCase()}`);
-      setSearchTerm("");
+
+    if (!searchTerm.trim()) {
+      setError("Whoops, can't be empty...");
+      return;
     }
+
+    setError("");
+    navigate(`/${searchTerm.toLowerCase()}`);
+    setSearchTerm("");
   };
 
   return (
@@ -24,7 +33,13 @@ const Search = () => {
       <div className="mt-6 md:mt-[50px] relative" role="search">
         <input
           id="search"
-          className="font-bold bg-gray-100 dark:bg-gray-800 border-gray-100 w-full h-12 rounded-2xl pl-5 md:h-16 md:text-heading-s"
+          className={`font-bold bg-gray-100 dark:bg-gray-800 border-gray-100 w-full h-12 rounded-2xl pl-5 md:h-16 md:text-heading-s focus-visible:outline-purple ${
+            error && "outline-1 outline-red!"
+          } ${
+            theme === "light"
+              ? "placeholder:text-gray-700"
+              : "placeholder:text-white"
+          } placeholder:opacity-25`}
           type="text"
           placeholder="Search for any word..."
           value={searchTerm}
@@ -32,6 +47,7 @@ const Search = () => {
           role="searchbox"
           aria-label="Search for word definitions"
           aria-describedby="search-button"
+          aria-invalid={!!error}
         />
         <button
           id="search-button"
@@ -42,6 +58,11 @@ const Search = () => {
           <SearchIcon aria-hidden="true" />
         </button>
       </div>
+      {error && (
+        <p className="text-red mt-2 md:text-heading-s" role="alert">
+          {error}
+        </p>
+      )}
     </form>
   );
 };
